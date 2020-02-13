f1ea8a9ebd9300f93324b19aa95a26ae3faf175b
Zeyad Etman <zeyadetman@gmail.com>
2020-02-13 12:30:40 +0200

    naïve solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { Tabs, Switch } from "antd";
 import "antd/dist/antd.css";
 import "./styles.css";
@@ -7,23 +7,18 @@ import AnotherCollapse from "./anotherCollapse";
 
 const { TabPane } = Tabs;
 export default function App() {
-  const conta = useRef(null);
-
-  useEffect(() => {
-    const cotaninerRef = conta.current;
-    // for(let x of containerRef)
-  }, []);
+  const [isDisable, setDisable] = useState(false);
   return (
     <>
       <Switch
-        checkedChildren="buttons enabled"
-        unCheckedChildren="buttons disabled"
-        onChange={console.log}
-        defaultChecked
+        checkedChildren="buttons disabled"
+        unCheckedChildren="buttons enabled"
+        onChange={setDisable}
+        defaultChecked={isDisable}
       />
       <Tabs defaultActiveKey="1" onChange={() => {}}>
         <TabPane tab="Tab 1" key="1">
-          <CustomCollapse tabName="Tab 1" />
+          <CustomCollapse tabName="Tab 1" isDisable={isDisable} />
         </TabPane>
         <TabPane tab="Tab 2" key="2">
           <CustomCollapse tabName="Tab 2" />
--- src/customCollapse.js
+++ src/customCollapse.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Collapse, Button } from "antd";
 
 const { Panel } = Collapse;
-const CustomCollapse = ({ tabName }) => {
+const CustomCollapse = ({ tabName, isDisable }) => {
   return (
     <Collapse defaultActiveKey={["1"]} onChange={console.log}>
       <Panel header="This is panel header 1" key="1">
-        <Button type="primary">{tabName} Panel 1</Button>
+        <Button type="primary" {...(isDisable ? { disabled: true } : {})}>
+          {tabName} Panel 1
+        </Button>
       </Panel>
       <Panel header="This is panel header 2" key="2">
         <Button type="primary">{tabName} Panel 2 (Don't Disable Me)</Button>
